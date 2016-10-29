976f846acd08c93dafaa5f0d7851b096406ff024
Merlin Gough <M.Gough@kainos.com>
2016-10-29 17:13:19 +0100

    Renamed handler method

--- alexaSkill/src/main/java/DMRequestStreamHandler.java
+++ alexaSkill/src/main/java/DMRequestStreamHandler.java
@@ -16,11 +16,11 @@ import java.util.Set;
 /**
  * This class could be the handler for an AWS Lambda function powering an Alexa Skills Kit
  * experience. To do this, simply set the handler field in the AWS Lambda console to
- * "helloworld.HelloWorldSpeechletRequestStreamHandler" For this to work, you'll also need to build
+ * "helloworld.DMRequestStreamHandler" For this to work, you'll also need to build
  * this project using the {@code lambda-compile} Ant task and upload the resulting zip file to power
  * your function.
  */
-public final class HelloWorldSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+public final class DMRequestStreamHandler extends SpeechletRequestStreamHandler {
     private static final Set<String> supportedApplicationIds = new HashSet<String>();
     static {
         /*
@@ -30,7 +30,7 @@ public final class HelloWorldSpeechletRequestStreamHandler extends SpeechletRequ
         supportedApplicationIds.add("amzn1.echo-sdk-ams.app.[unique-value-here]");
     }
 
-    public HelloWorldSpeechletRequestStreamHandler() {
+    public DMRequestStreamHandler() {
         super(new HelloWorldSpeechlet(), supportedApplicationIds);
     }
 }
