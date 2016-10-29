30515dd39b67d4af9da9766207e380ccd2696d48
clegganator259 <djclegg01@hotmail.com>
2016-10-30 00:23:40 +0100

    Fiddled with package.json and fixed body parsing

--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ var port = 8080;
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
-app.use(bodyParser.json));       // to support JSON-encoded bodies
-app.use(express.json());
+app.use(bodyParser.json);
 
 var map = {
   width: 10,
